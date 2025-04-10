fbf0c7563c82236429faa3f1dc11f48b2fe416bb
Rafael Carvalho <faeel-xd@hotmail.com>
2025-04-10 15:16:29 -0300

    Testando controller

--- src/main/java/com/example/personal_trainer_app/controller/LoginController.java
+++ src/main/java/com/example/personal_trainer_app/controller/LoginController.java
@@ -1,11 +1,14 @@
 package com.example.personal_trainer_app.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@GetMapping("/login")
+@RequestMapping("/login")
 public class LoginController {
 
-    
+    public String Hello() {
+        return "Hello, this is the login controller!";
+    }
 
 }
